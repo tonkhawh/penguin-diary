bcf1290059c6db8ed28414d825a8a8376dd1765c
tonkhawh <tonkhawh@gmail.com>
2026-09-07 09:14:42 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'penguin-diary-v1';
+const CACHE_NAME = 'penguin-diary-v2';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
